7b741955f37f8979275f23ede39c41edd3e6cf27
Esther <esthersweeting@outlook.com>
2026-09-05 17:22:56 +0100

    Modified product.js to load extra products from the products array and update a basket in local storage

--- frontend/javaScript/product.js
+++ frontend/javaScript/product.js
@@ -1,5 +1,6 @@
 import {formatPrice} from "./functions.js";
-import {sizes} from "./products.js";
+import {sizes, products} from "./products.js";
+let clickedSizeIndex= 0;
 let currentProduct = JSON.parse(localStorage.getItem("currentProduct"));
 
 const type = document.getElementById("p-link-type-id");
@@ -26,6 +27,7 @@ function setupSizeButton (size,index){
     btn.id = "g-" + size;
     btn.addEventListener("click",() => {
         price.textContent=formatPrice( currentProduct.price[index]);
+        clickedSizeIndex = index;
     });
     sizeButtons.appendChild(btn);
     
@@ -33,5 +35,59 @@ function setupSizeButton (size,index){
 
 
 sizes.forEach(setupSizeButton);
+
+const basketButton = document.getElementById("p-add-id");
+
+function addToBasket(){
+    const quantity = document.getElementById("p-quantity-id").value;
+    const basket = localStorage.getItem("basket");
+    const basketItem = [currentProduct,clickedSizeIndex,quantity];
+    
+    if (basket == null){
+        localStorage.setItem("basket",JSON.stringify([basketItem]));
+    }
+    else{
+        console.log(basket);
+        const newBasket = JSON.parse(basket);
+        newBasket.push(basketItem);
+        localStorage.setItem("basket",JSON.stringify(newBasket));
+    }
+    
+}
+basketButton.onclick= addToBasket;
+
+const moreProducts = document.getElementById("p-extra-products-id");
+
+function createExtraProduct (product){
+    let div = document.createElement("div");
+    div.className= "p-extra-product";
+
+    const img = document.createElement("img");
+    img.src = "../images/temp.jpg";
+
+    const extraName = document.createElement("p");
+    extraName.textContent=product.name;
+
+    div.onclick = function (){
+        localStorage.setItem("currentProduct",JSON.stringify(product));
+        location.href = "../html/product.html";
+    };
+
+    div.appendChild(img);
+    div.appendChild(extraName);
+    moreProducts.appendChild(div);
+
+}
+
+for (let i = 0; i<4; i++){
+    createExtraProduct(products[i]);
+}
+
+//<div class="p-extra-product">
+//           <img src="../images/temp.jpg">
+//           <p> Rhubarb bonbons</p>
+//         </div>
+
+
 
 
