75e1242244d4ec074ad41be4bf9235ee540c870e
Esther <esthersweeting@outlook.com>
2026-09-12 18:26:03 +0100

    Updated so the buttons on index.html work

---
+++ frontend/javaScript/indexController.js
@@ -0,0 +1,9 @@
+const shopButton=document.getElementById("i-enter-shop");
+shopButton.onclick= function (){
+    location.href="../html/shop.html";
+};
+
+const aboutButton=document.getElementById("i-view-profile");
+aboutButton.onclick= function (){
+    location.href="../html/about-me.html";
+};
